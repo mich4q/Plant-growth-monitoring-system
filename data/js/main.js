a15abb1f1e6a901e31e46eb7b9d1d4ca495026d2
mishyu <mich4lrytel@gmail.com>
2024-11-18 23:31:45 +0100

    Fronciak stuff

---
+++ data/js/main.js
@@ -0,0 +1,34 @@
+$(function () {
+    $("#wifiModeSwitch").on('change', function () {
+        $('#ssid').val('');
+        $('#password').val('');
+    });
+
+    $('#save_network').on('click', function (event) {
+        saveNetwork();
+    });
+})
+
+function saveNetwork() {
+    const data = {
+        ssid: $('#ssid').val(),
+        password: $('#password').val(),
+        wifiMode: $('input[name="mode"]:checked').val(),
+    };
+
+    $.ajax({
+        url: '/savenetwork',
+        type: 'GET',
+        contentType: 'application/json',
+        data: data,
+        success: function (response) {
+            console.log('data saved');
+            console.log(data);
+
+        },
+        error: function (xhr, status, error) {
+            console.log('error occured: ' + error);
+        }
+    });
+}
+
